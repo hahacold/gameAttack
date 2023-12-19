e723485cf0bdcc07c16e384b0af4af65b13bd0a4
hahacold <taiwanhank0908@gmail.com>
2023-12-20 01:30:39 +0800

    Initial Commit

---
+++ gameAttack/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  gameAttack
+//
+//  Created by test on 2023/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ gameAttack/gameAttackApp.swift
@@ -0,0 +1,17 @@
+//
+//  gameAttackApp.swift
+//  gameAttack
+//
+//  Created by test on 2023/12/20.
+//
+
+import SwiftUI
+
+@main
+struct gameAttackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
